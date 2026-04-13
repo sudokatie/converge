8a1f082aec65da48a448665db18f83acf0dfa063
Katie <everthejetsets@gmail.com>
2026-04-13 04:31:26 -0500

    add creature entity system with types and queries

--- crates/game/src/entities/mod.rs
+++ crates/game/src/entities/mod.rs
@@ -1,5 +1,7 @@
 //! Entity factory functions.
 
+mod creature;
 mod player;
 
+pub use creature::{query_creatures, query_hostile, query_passive, spawn_creature, Creature, CreatureKind};
 pub use player::spawn_player;
