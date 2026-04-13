d8c55095bfa1edc1a11f384961fbcb8e178e140c
Katie <everthejetsets@gmail.com>
2026-04-13 15:32:28 -0500

    add tree/cactus structure generation

--- crates/engine_world/src/generation/mod.rs
+++ crates/engine_world/src/generation/mod.rs
@@ -3,9 +3,11 @@
 mod biome;
 mod caves;
 mod noise;
+mod structures;
 mod terrain;
 
 pub use biome::{Biome, BiomeSelector};
 pub use caves::CaveCarver;
 pub use noise::TerrainNoise;
+pub use structures::{should_place_tree, structure_random, Structure, StructureBlock};
 pub use terrain::TerrainGenerator;
