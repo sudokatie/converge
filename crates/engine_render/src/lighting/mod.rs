eb945b6bca06a0bf081191ef31e17d6e30778e4a
Katie <everthejetsets@gmail.com>
2026-04-17 17:31:56 -0500

    add light update manager for invalidation on block changes

--- crates/engine_render/src/lighting/mod.rs
+++ crates/engine_render/src/lighting/mod.rs
@@ -6,9 +6,14 @@
 mod block_light;
 mod directional;
 mod light_uniform;
+mod light_update;
 mod point_light;
 
 pub use block_light::{BlockLightMap, LightValue, LIGHT_MAX};
 pub use directional::DirectionalLight;
 pub use light_uniform::{LightUniform, LightUniformBuffer};
+pub use light_update::{
+    LightUpdate, LightUpdateManager, affected_positions, is_affected,
+    LIGHT_INVALIDATION_RADIUS,
+};
 pub use point_light::{PointLight, PointLightManager, MAX_POINT_LIGHTS};
