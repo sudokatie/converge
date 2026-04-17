396080018234a4079a504393e33d6c62d8bbf4b2
Katie <everthejetsets@gmail.com>
2026-04-17 15:14:42 -0500

    add debug console with command history and builtin command processor

--- crates/engine_ui/src/hud/mod.rs
+++ crates/engine_ui/src/hud/mod.rs
@@ -1,12 +1,16 @@
 //! In-game HUD elements.
 
 mod crosshair;
+mod debug_console;
 mod debug_overlay;
 mod health_bar;
 mod hotbar;
 mod hunger_bar;
 
 pub use crosshair::{CrosshairConfig, CrosshairStyle, draw_crosshair};
+pub use debug_console::{
+    process_builtin_command, ConsoleAction, ConsoleLine, DebugConsole, LineKind,
+};
 pub use debug_overlay::{DebugLevel, DebugOverlay, DebugStats};
 pub use health_bar::{draw_health_bar, HealthBarState};
 pub use hotbar::{draw_hotbar, HotbarSlot, ItemTextures};
